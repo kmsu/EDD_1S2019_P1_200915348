914439e1b20d4c80335c53031c02b13adc2c33b0
kmsu <34343298+kmsu@users.noreply.github.com>
2019-08-09 00:29:47 -0600

    Update menu.py

--- Practica 1/menu.py
+++ Practica 1/menu.py
@@ -3,106 +3,107 @@ import csv
 import os.path
 from users import circular
 from users import node
-usersList = circular()
+userList = circular()
 
 menu = ["Home", "Play", "Scoreboard", "User selection", "Reports", "Bulk loading", "Exit"]
-submenu = ["Yes", "No"]
+user_in_game = None
 
-def print_menu(stdscr, selected_row_idx):
-    stdscr.clear()
-    h, w = stdscr.getmaxyx()
+def paint_title(win,var):
+    win.clear()                         #it's important to clear the screen because of new functionality everytime we call this function
+    win.border(0)                       #after clearing the screen we need to repaint the border to keep track of our working area
+    x_start = round((60-len(var))/2)    #center the new title to be painted
+    win.addstr(0,x_start,var)
+
+def paint_menu(win, selected_row_idx):
+    paint_title(win,' MAIN MENU ')          #paint title
     for idx, row in enumerate(menu): #enumerate retorna en idx la posicion y en row el texto en esa posicion
-        x = w//2 - len(row)//2
-        y = h//2 - len(menu)//2 + idx
+        x = round((60-len(row))/2)
+        y = 20//2 - len(menu)//2 + idx
         if idx == selected_row_idx:
-            stdscr.attron(curses.color_pair(1))
-            stdscr.addstr(y, x, row) #add text to show
-            stdscr.attroff(curses.color_pair(1))
+            win.attron(curses.color_pair(1))
+            win.addstr(y, x, row)
+            win.attroff(curses.color_pair(1))
         else:
-            stdscr.addstr(y, x, row)
-    stdscr.refresh()
+            win.addstr(y, x, row)
 
-def print_sub_menu(stdscr, selected_row_idx):
-    stdscr.clear()
-    h, w = stdscr.getmaxyx()
-    for idx, row in enumerate(submenu): #enumerate retorna en idx la posicion y en row el texto en esa posicion
-        x = w//2 - len(row)//2 + (idx*(len(row)+2))
-        y = h//2 - len(menu)//2
-        if idx == selected_row_idx:
-            stdscr.attron(curses.color_pair(1))
-            stdscr.addstr(y, x, row) #add text to show
-            stdscr.attroff(curses.color_pair(1))
-        else:
-            stdscr.addstr(y, x, row)
-    stdscr.refresh()
+def print_center(win, text):
+    #win.clear()
+    #paint_title(win, text)
+    x = 60//2 - len(text)//2
+    y = 20//2
+    win.addstr(y, x, text)
+    win.refresh()
 
-def print_center(stdscr, text):
-    stdscr.clear()
-    h, w = stdscr.getmaxyx()
-    x = w//2 - len(text)//2
-    y = h//2
-    stdscr.addstr(y, x, text)
-    stdscr.refresh()
+def print_user(win, text, varTitle):
+    paint_title(win, varTitle)
+    x = 60//2 - len(text)//2
+    y = 20//2
+    win.addstr(y, x, text)
+    win.refresh()
 
 def main(stdscr):
-    # turn off cursor blinking
+    curses.initscr()
     curses.curs_set(0)
-    # color scheme for selected row
+    height = 20
+    width = 60
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    # specify the current selected row
+    curses.noecho()
+    #key = 0
     current_row = 0
-    # print the menu
-    print_menu(stdscr, current_row)
 
-    nodeUser = usersList.getHead()
-    while 1:
-        key = stdscr.getch()
-        #nodeUser = usersList.getHead()
-        #user = nodeUser
+    s = curses.newwin(height, width, 0, 0)
+    s.border(0)
+    s.keypad(True)
+    paint_menu(s, current_row)
+
+    while(True):
+        key = s.getch()
+
         if key == curses.KEY_UP and current_row > 0:
             current_row -= 1
-            print_menu(stdscr, current_row)
+            paint_menu(s, current_row)
         elif key == curses.KEY_DOWN and current_row < len(menu)-1:
             current_row += 1
-            print_menu(stdscr, current_row)
+            paint_menu(s, current_row)
         elif key == curses.KEY_ENTER or key in [10, 13]:
-            #print_center(stdscr, "You selected '{}'".format(menu[current_row]))
-            #stdscr.getch()
             if current_row == 0:
-                print_center(stdscr, "regresar a home")
-                #print_sub_menu(stdscr, 0)
+                paint_title(s, "Home")
+                print("Home")
             elif current_row == 1:
-                print_center(stdscr, "iniciar juego")
+                pass
             elif current_row == 2:
-                print_center(stdscr, "Scoreboard")
+                pass
             elif current_row == 3:
-                print_center(stdscr, "user selection")
-                nodeUser = usersList.getHead()
+                paint_title(s, "User selection")
+                nodeUser = userList.getHead()
                 if nodeUser is None:
-                    print_center(stdscr, "users not found")
+                    print_center(s, "users not found")
                 else:
-                    print_center(stdscr, nodeUser.data)
-                    
+                    print_center(s, "<- " + nodeUser.data + " ->")
+                    while 1:
+                        key = s.getch()
+                        if key == curses.KEY_LEFT:
+                            user = nodeUser.previous
+                            print_user(s, "<- " + user.data + " ->", "User selection")
+                            nodeUser = user
+                        elif key == curses.KEY_RIGHT:
+                            user = nodeUser.next
+                            print_user(s, "<- " + user.data + " ->", "User selection")
+                            nodeUser = user
+                        elif key == curses.KEY_ENTER or key in [10, 13]:
+                            user_in_game = nodeUser.data
+                            break
+                    paint_menu(s, current_row)
             elif current_row == 4:
-                print_center(stdscr, "Reports")
+                pass
             elif current_row == 5:
-                print_center(stdscr, "Bulk loading")
-                usersList.archivo()
-                #usersList.print_list()
-            # if user selected last row, exit the program
-            elif current_row == len(menu)-1:
-                print_center(stdscr, "finalizar juego")
+                paint_title(s, "Bulk loading")
+                print_center(s, "data loaded successfully")
+                userList.archivo()
+            elif key == curses.KEY_ENTER or key in [10, 13] and current_row == len(menu)-1:
                 break
-            else:
-                print_menu(stdscr, current_row)
 
-        elif key == curses.KEY_LEFT:
-            user = nodeUser.previous
-            print_center(stdscr, user.data)
-            nodeUser = user
-        elif key == curses.KEY_RIGHT:
-            user = nodeUser.next
-            print_center(stdscr, user.data)
-            nodeUser = user
+        s.refresh()
+    curses.endwin()
 
 curses.wrapper(main)
